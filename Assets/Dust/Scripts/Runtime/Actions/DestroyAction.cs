e857cdd09218d248d63df085509261953e3d17ea
Andrii Bakulin <andrii.bakulin@gmail.com>
2024-01-05 00:19:35 -0800

    DestroyerAction: minor fix

--- Assets/Dust/Scripts/Runtime/Actions/DestroyAction.cs
+++ Assets/Dust/Scripts/Runtime/Actions/DestroyAction.cs
@@ -45,12 +45,12 @@ namespace Dust
 
         public void DestroyNow()
         {
-            foreach (var comp in components.Where(comp => !Dust.IsNull(comp)))
+            foreach (var comp in components.Where(comp => Dust.IsNotNull(comp)))
             {
                 Destroy(comp);
             }
 
-            foreach (var go in gameObjects.Where(go => !Dust.IsNull(go)))
+            foreach (var go in gameObjects.Where(go => Dust.IsNotNull(go)))
             {
                 Destroy(go);
             }
