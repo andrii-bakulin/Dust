c658c18585f0017d75c67e7d93a2a23e25a70176
Andrii Bakulin <andrii.bakulin@gmail.com>
2024-05-12 15:39:03 -0700

    StartAction: Added static method Play to auto-search and play start action

--- Assets/Dust/Scripts/Runtime/Actions/StartAction.cs
+++ Assets/Dust/Scripts/Runtime/Actions/StartAction.cs
@@ -5,6 +5,20 @@ namespace Dust
     [AddComponentMenu("Dust/Actions/Start Action")]
     public class StartAction : InstantAction
     {
+        public static bool Play(GameObject source)
+        {
+            if (Dust.IsNull(source))
+                return false;
+
+            var startAction = source.GetComponent<StartAction>();
+
+            if (Dust.IsNull(startAction))
+                return false;
+
+            startAction.Play();
+            return true;
+        }
+
         //--------------------------------------------------------------------------------------------------------------
         // Dust.Action lifecycle
 
